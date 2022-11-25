49ef59a9e48042535e2281f57561b9aacf54c2a7
Teejay39 <63671611+Teejay39@users.noreply.github.com>
2022-11-25 17:47:05 +0100

    change font

--- TOR Installer/Form1.Designer.cs
+++ TOR Installer/Form1.Designer.cs
@@ -47,10 +47,10 @@ namespace TOR_Installer
             // pictureBox1
             // 
             this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
-            this.pictureBox1.Location = new System.Drawing.Point(14, 14);
-            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.pictureBox1.Location = new System.Drawing.Point(16, 18);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(324, 72);
+            this.pictureBox1.Size = new System.Drawing.Size(370, 91);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
@@ -60,11 +60,11 @@ namespace TOR_Installer
             // 
             // button1
             // 
-            this.button1.Font = new System.Drawing.Font("Moorley Hoosten", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.button1.Location = new System.Drawing.Point(14, 121);
-            this.button1.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.button1.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.button1.Location = new System.Drawing.Point(16, 153);
+            this.button1.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(245, 32);
+            this.button1.Size = new System.Drawing.Size(280, 41);
             this.button1.TabIndex = 2;
             this.button1.Text = "Pfad zu Among Us auswaehlen";
             this.button1.UseVisualStyleBackColor = true;
@@ -73,12 +73,12 @@ namespace TOR_Installer
             // error_pfad
             // 
             this.error_pfad.AutoSize = true;
-            this.error_pfad.Font = new System.Drawing.Font("Moorley Hoosten", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.error_pfad.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
             this.error_pfad.ForeColor = System.Drawing.Color.Red;
-            this.error_pfad.Location = new System.Drawing.Point(266, 125);
-            this.error_pfad.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.error_pfad.Location = new System.Drawing.Point(304, 158);
+            this.error_pfad.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
             this.error_pfad.Name = "error_pfad";
-            this.error_pfad.Size = new System.Drawing.Size(76, 20);
+            this.error_pfad.Size = new System.Drawing.Size(79, 19);
             this.error_pfad.TabIndex = 3;
             this.error_pfad.Text = "{{ERROR}}";
             this.error_pfad.Visible = false;
@@ -86,23 +86,23 @@ namespace TOR_Installer
             // error_tor_path
             // 
             this.error_tor_path.AutoSize = true;
-            this.error_tor_path.Font = new System.Drawing.Font("Moorley Hoosten", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.error_tor_path.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
             this.error_tor_path.ForeColor = System.Drawing.Color.Red;
-            this.error_tor_path.Location = new System.Drawing.Point(266, 164);
-            this.error_tor_path.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.error_tor_path.Location = new System.Drawing.Point(304, 208);
+            this.error_tor_path.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
             this.error_tor_path.Name = "error_tor_path";
-            this.error_tor_path.Size = new System.Drawing.Size(76, 20);
+            this.error_tor_path.Size = new System.Drawing.Size(79, 19);
             this.error_tor_path.TabIndex = 5;
             this.error_tor_path.Text = "{{ERROR}}";
             this.error_tor_path.Visible = false;
             // 
             // button2
             // 
-            this.button2.Font = new System.Drawing.Font("Moorley Hoosten", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.button2.Location = new System.Drawing.Point(14, 160);
-            this.button2.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.button2.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.button2.Location = new System.Drawing.Point(16, 203);
+            this.button2.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(245, 32);
+            this.button2.Size = new System.Drawing.Size(280, 41);
             this.button2.TabIndex = 4;
             this.button2.Text = "Wo soll TOR Installiert werden?";
             this.button2.UseVisualStyleBackColor = true;
@@ -111,33 +111,33 @@ namespace TOR_Installer
             // DownloadBar
             // 
             this.DownloadBar.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(153)))), ((int)(((byte)(204)))), ((int)(((byte)(102)))));
-            this.DownloadBar.Location = new System.Drawing.Point(14, 365);
-            this.DownloadBar.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.DownloadBar.Location = new System.Drawing.Point(16, 462);
+            this.DownloadBar.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
             this.DownloadBar.Name = "DownloadBar";
-            this.DownloadBar.Size = new System.Drawing.Size(530, 27);
+            this.DownloadBar.Size = new System.Drawing.Size(606, 34);
             this.DownloadBar.TabIndex = 10;
             this.DownloadBar.Visible = false;
             // 
             // status
             // 
             this.status.AutoSize = true;
-            this.status.Font = new System.Drawing.Font("Moorley Hoosten", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.status.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
             this.status.ForeColor = System.Drawing.Color.Transparent;
-            this.status.Location = new System.Drawing.Point(14, 338);
-            this.status.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.status.Location = new System.Drawing.Point(16, 428);
+            this.status.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
             this.status.Name = "status";
-            this.status.Size = new System.Drawing.Size(83, 20);
+            this.status.Size = new System.Drawing.Size(83, 19);
             this.status.TabIndex = 11;
             this.status.Text = "{{STATUS}}";
             this.status.Visible = false;
             // 
             // button3
             // 
-            this.button3.Font = new System.Drawing.Font("Moorley Hoosten", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
-            this.button3.Location = new System.Drawing.Point(13, 222);
-            this.button3.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.button3.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.button3.Location = new System.Drawing.Point(15, 281);
+            this.button3.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(245, 32);
+            this.button3.Size = new System.Drawing.Size(280, 41);
             this.button3.TabIndex = 12;
             this.button3.Text = "Installation Starten";
             this.button3.UseVisualStyleBackColor = true;
@@ -146,12 +146,12 @@ namespace TOR_Installer
             // error_install
             // 
             this.error_install.AutoSize = true;
-            this.error_install.Font = new System.Drawing.Font("Moorley Hoosten", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.error_install.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
             this.error_install.ForeColor = System.Drawing.Color.Red;
-            this.error_install.Location = new System.Drawing.Point(13, 258);
-            this.error_install.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.error_install.Location = new System.Drawing.Point(15, 327);
+            this.error_install.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
             this.error_install.Name = "error_install";
-            this.error_install.Size = new System.Drawing.Size(76, 20);
+            this.error_install.Size = new System.Drawing.Size(79, 19);
             this.error_install.TabIndex = 13;
             this.error_install.Text = "{{ERROR}}";
             this.error_install.Visible = false;
@@ -162,9 +162,10 @@ namespace TOR_Installer
             this.button4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.button4.Font = new System.Drawing.Font("Arial Narrow", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
             this.button4.ForeColor = System.Drawing.Color.White;
-            this.button4.Location = new System.Drawing.Point(515, 12);
+            this.button4.Location = new System.Drawing.Point(589, 15);
+            this.button4.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(31, 40);
+            this.button4.Size = new System.Drawing.Size(35, 51);
             this.button4.TabIndex = 14;
             this.button4.Text = "X";
             this.button4.UseVisualStyleBackColor = true;
@@ -174,9 +175,10 @@ namespace TOR_Installer
             // 
             this.MovePanel.BackColor = System.Drawing.Color.Transparent;
             this.MovePanel.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.MovePanel.Location = new System.Drawing.Point(-9, -4);
+            this.MovePanel.Location = new System.Drawing.Point(-10, -5);
+            this.MovePanel.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.MovePanel.Name = "MovePanel";
-            this.MovePanel.Size = new System.Drawing.Size(571, 56);
+            this.MovePanel.Size = new System.Drawing.Size(653, 71);
             this.MovePanel.TabIndex = 15;
             this.MovePanel.MouseDown += new System.Windows.Forms.MouseEventHandler(this.MovePanel_MouseDown);
             this.MovePanel.MouseMove += new System.Windows.Forms.MouseEventHandler(this.MovePanel_MouseMove);
@@ -184,10 +186,10 @@ namespace TOR_Installer
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 19F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(33)))), ((int)(((byte)(37)))), ((int)(((byte)(41)))));
-            this.ClientSize = new System.Drawing.Size(558, 405);
+            this.ClientSize = new System.Drawing.Size(638, 513);
             this.Controls.Add(this.MovePanel);
             this.Controls.Add(this.button4);
             this.Controls.Add(this.error_install);
@@ -199,9 +201,10 @@ namespace TOR_Installer
             this.Controls.Add(this.error_pfad);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.pictureBox1);
+            this.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
-            this.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.Margin = new System.Windows.Forms.Padding(5, 4, 5, 4);
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Tor Downloader";
